401975177fd0ae9b69fbf48b3617df84c503ce0d
Martim Brandao <martim@robots.ox.ac.uk>
2019-07-18 11:54:50 +0100

    fsc file as param

--- recast_demos/src/gridmap_fsc_demo_node.cpp
+++ recast_demos/src/gridmap_fsc_demo_node.cpp
@@ -41,51 +41,50 @@ int main (int argc, char* argv[])
   ros::NodeHandle nodeHandle("~");
 
   // params
-  const std::string strEnvironment("fsc");
   double resolution;
+  std::string base = ros::package::getPath("recast_demos");
+  std::string path;
   nodeHandle.param("resolution", resolution, 0.05);
+  nodeHandle.param("path", path, base+std::string("/data/fsc-oil-rig-map-full-1cm-clean.pcd"));
 
   // gridmap
   grid_map::GridMap gridmap({"elevation"});
 
-  // load
-  if (strEnvironment == "fsc") {
-    // load PCL file
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud( new pcl::PointCloud<pcl::PointXYZRGB>() );
-    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/martim/Downloads/fsc-oil-rig-map-full-1cm-clean.pcd", *cloud) == -1) {
-      PCL_ERROR("Couldn't read .pcd file \n");
-      ROS_ERROR("Couldn't read .pcd file \n");
-      return -1;
+  // load PCL file
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud( new pcl::PointCloud<pcl::PointXYZRGB>() );
+  if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (path, *cloud) == -1) {
+    PCL_ERROR("Couldn't read .pcd file \n");
+    ROS_ERROR("Couldn't read .pcd file \n");
+    return -1;
+  }
+  // chop off top floor
+  ROS_INFO("Cropping...");
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered( new pcl::PointCloud<pcl::PointXYZRGB>() );
+  //pcl::PassThrough<pcl::PointXYZRGB> pass;
+  //pass.setInputCloud (cloud);
+  //pass.setFilterFieldName ("z");
+  //pass.setFilterLimits (-10.0, 1.0);
+  //pass.filter (*cloud_filtered);
+  for (unsigned int i = 0; i < cloud->points.size(); i++) {
+    const pcl::PointXYZRGB &pt = cloud->points[i];
+    if (pt.x >=   0.0 && pt.x <= 40.0 &&
+        pt.y >=   0.0 && pt.y <= 80.0 &&
+        pt.z >= -10.0 && pt.z <=  1.0) {
+      cloud_filtered->points.push_back(pt);
     }
-    // chop off top floor
-    ROS_INFO("Cropping...");
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered( new pcl::PointCloud<pcl::PointXYZRGB>() );
-    //pcl::PassThrough<pcl::PointXYZRGB> pass;
-    //pass.setInputCloud (cloud);
-    //pass.setFilterFieldName ("z");
-    //pass.setFilterLimits (-10.0, 1.0);
-    //pass.filter (*cloud_filtered);
-    for (unsigned int i = 0; i < cloud->points.size(); i++) {
-      const pcl::PointXYZRGB &pt = cloud->points[i];
-      if (pt.x >=   0.0 && pt.x <= 40.0 &&
-          pt.y >=   0.0 && pt.y <= 80.0 &&
-          pt.z >= -10.0 && pt.z <=  1.0) {
-        cloud_filtered->points.push_back(pt);
-      }
-    }
-    // convert to gridmap
-    ROS_INFO("Converting to grid_map...");
-    convert(cloud_filtered, gridmap, resolution);
-    // fix some issues with the map
-    for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(9.65,22.3), 0.70); !iterator.isPastEnd(); ++iterator) { // close to stairs
-      gridmap.at("elevation", *iterator) = -1.28;
-    }
-    for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(10.6,32.1), 0.30); !iterator.isPastEnd(); ++iterator) { // close to stairs bottom
-      gridmap.at("elevation", *iterator) = -1.38;
-    }
-    for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(10,36.0), 0.80); !iterator.isPastEnd(); ++iterator) { // close to slab
-      gridmap.at("elevation", *iterator) = -1.40;
-    }
+  }
+  // convert to gridmap
+  ROS_INFO("Converting to grid_map...");
+  convert(cloud_filtered, gridmap, resolution);
+  // fix some issues with the map
+  for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(9.65,22.3), 0.70); !iterator.isPastEnd(); ++iterator) { // close to stairs
+    gridmap.at("elevation", *iterator) = -1.28;
+  }
+  for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(10.6,32.1), 0.30); !iterator.isPastEnd(); ++iterator) { // close to stairs bottom
+    gridmap.at("elevation", *iterator) = -1.38;
+  }
+  for (grid_map::SpiralIterator iterator(gridmap, grid_map::Position(10,36.0), 0.80); !iterator.isPastEnd(); ++iterator) { // close to slab
+    gridmap.at("elevation", *iterator) = -1.40;
   }
 
   // publish
